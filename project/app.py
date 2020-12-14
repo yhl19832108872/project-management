88c0c31d60a0046b42d49dc03e3350ab9f1d47a8
duyuxn <jslscstg_007@163.com>
2020-12-14 21:55:20 +0800

    htmls

--- project/app.py
+++ project/app.py
@@ -121,7 +121,7 @@ def fail():
     return render_template('fail.html')
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run()
+    app.run(debug=True)
+    #app.run()
     db.create_all()
 
